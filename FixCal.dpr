dd524d330d58d7c172f1c36c1aa807bd9f21d4c2
Remko Weijnen <r.weijnen@gmail.com>
2018-09-10 23:43:18 +0200

    Create FixCal.dpr

---
+++ FixCal.dpr
@@ -0,0 +1,14 @@
+program FixCal;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Fix Calendar';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
